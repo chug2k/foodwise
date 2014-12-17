8c74433377cb335916ec4018db415bf59dc72922
Charles Lee <chug2k@gmail.com>
2014-12-16 16:16:18 -0800

    Fixing routes to use the URL helper and not hardcode in /admin.

--- admin/foodwise_admin.rb
+++ admin/foodwise_admin.rb
@@ -7,20 +7,49 @@ module Foodwise
         :expire_after => 31557600, # 1 year
         :secret       => ENV['SESSION_SECRET'] || 'Lumpy Space Princess'
 
+
+    helpers do
+      def login?
+        if session[:user_id].nil?
+          return false
+        else
+          return true
+        end
+      end
+
+
+      def current_user
+        return session[:user_id]
+      end
+
+    end
+
+
     get '/' do
       slim :login
     end
 
+    post '/login' do
+      puts "Params are: #{params}"
+
+      # session[:username] = params[:username]
+    end
+
+
     # TODO(Charles): If Admin ever feels slow, consider serving assets from Sprockets.
     get '/js/*.js' do
       filename = params[:splat].first
-      coffee "../public/js/#{filename}".to_sym
-    end
-    get '/css/*.css' do
-      filename = params[:splat].first
-      stylus "../public/css/#{filename}".to_sym
+      if File.exist?("../public/js//#{viewname}.coffee")
+        coffee "../public/js/#{filename}".to_sym
+      end
     end
 
+    get '/css/*.css' do
+      filename = params[:splat].first
+      if File.exist?("../public/css/#{filename}.styl")
+        stylus "../public/css/#{filename}".to_sym
+      end
+    end
 
   end
 end
--- config.ru
+++ config.ru
@@ -7,7 +7,6 @@ require './models/user'
 require './api/foodwise_api.rb'
 require './admin/foodwise_admin.rb'
 
-use Rack::Coffee, root: '/admin/public', urls: '/js'
 
 run Rack::URLMap.new('/api' => Foodwise::Api.new, '/admin' => Foodwise::Admin.new)
 
